954393ff1759b1dd4213b5b0641e82fb723d3f4c
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-08-13 18:30:57 +0300

    feat: done microtasks for react router dom

---
+++ src/components/pages/Model.tsx
@@ -0,0 +1,41 @@
+import {adidasModels, SneakerType} from "./Adidas";
+import * as React from 'react';
+import {useParams} from "react-router-dom";
+import styles from './pages.module.css'
+import {pumaModels} from "./Puma";
+
+type SneakersModelsType = {
+    [key: string]: SneakerType[]
+}
+
+const sneakersModels: SneakersModelsType = {
+    adidas: adidasModels,
+    puma: pumaModels,
+}
+
+type Props = {};
+export const Model = (props: Props) => {
+    const {model, id} = useParams()
+    // console.log(params)
+
+    const currentModel = model
+        ? sneakersModels[model].find(el => el.id === Number(id))
+        : null
+
+    return (
+        <div className={styles.blockModel}>
+            {
+                currentModel
+                    ? <>
+                        <h3>{currentModel.model}</h3>
+                        <h4>Collection {currentModel.collection}</h4>
+                        <h4>Price {currentModel.price}</h4>
+                        <img className={styles.imageModel}
+                             src={currentModel.picture}
+                             alt={currentModel.model}/>
+                      </>
+                    : <h2>No model found</h2>
+            }
+        </div>
+    );
+};
