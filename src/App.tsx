17600c2a940d83626f44d0378451ec95407431e8
Dmitriy Kutsilin <dmitriy.kutsilin@gmail.com>
2024-10-15 20:34:53 +0300

    refactoring for working with react-router-dom 6.4 syntax, add "home" and "back" buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,55 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from "./components/Site.module.css";
-import {Adidas} from "./components/pages/Adidas";
-import {Puma} from "./components/pages/Puma";
-import {Abibas} from "./components/pages/Abibas";
-import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
-import {Error404} from "./components/pages/Error404";
+import {Link, NavLink, Outlet, useNavigate} from 'react-router-dom';
 import {S} from "./components/pages/_styles";
-import { Model } from './components/pages/Model';
-import {Prices} from "./components/pages/Prices";
+import {PATH} from "./routes/router";
 
-const PATH = {
-    PAGE1: '/adidas',
-    PAGE2: '/puma',
-    PAGE3: '/abibas',
-    PAGE4: '/prices',
-} as const
 
 function App() {
+    const navigate = useNavigate()
+    const navigateHandler = () => {
+        navigate(-1)
+    }
+
     return (
         <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.header}>
+                <h1>HEADER</h1>
+            </div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <S.NewWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NewWrapper>
-                    <S.NewWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NewWrapper>
-                    <S.NewWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NewWrapper>
-                    <S.NewWrapper><NavLink to={PATH.PAGE4}>Цены для оптовиков</NavLink></S.NewWrapper>
+                    <S.NewWrapper><NavLink to={PATH.ADIDAS}>Adidas</NavLink></S.NewWrapper>
+                    <S.NewWrapper><NavLink to={PATH.PUMA}>Puma</NavLink></S.NewWrapper>
+                    <S.NewWrapper><NavLink to={PATH.ABIBAS}>Abibas</NavLink></S.NewWrapper>
+                    <S.NewWrapper><NavLink to={PATH.PRICES}>Цены для оптовиков</NavLink></S.NewWrapper>
+                    <S.NewWrapper><NavLink to={PATH.PROTECTED}>ProtectedPage</NavLink></S.NewWrapper>
                 </div>
                 <div className={styles.content}>
-                    <Routes>
-                        <Route path="/" element={<Navigate to={PATH.PAGE1}/>}/>
-                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
-                        <Route path={PATH.PAGE2} element={<Puma/>}/>
-                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
-                        <Route path={PATH.PAGE4} element={<Prices/>}/>
+                    {/*навигацию к адидас по дефолту можно сделать и в массиве public routes */}
+                    {/*<Navigate to={PATH.ADIDAS}/>*/}
+                    <div className={styles.HorizontalNavigation}>
+                        <Link to={PATH.ADIDAS} className={styles.LinkLikeButton}>
+                            Главная страница (ADIDAS)
+                        </Link>
+                        <button onClick={navigateHandler} className={styles.ButtonLikeLink}>НАЗАД</button>
+                    </div>
+                    <Outlet/>
+                    {/*<Routes>*/}
+                    {/*    <Route path="/" element={<Navigate to={PATH.PAGE1}/>}/>*/}
+                    {/*    <Route path={PATH.PAGE1} element={<Adidas/>}/>*/}
+                    {/*    <Route path={PATH.PAGE2} element={<Puma/>}/>*/}
+                    {/*    <Route path={PATH.PAGE3} element={<Abibas/>}/>*/}
+                    {/*    <Route path={PATH.PAGE4} element={<Prices/>}/>*/}
 
-                        <Route path={'/:model/:id'} element={<Model/>}/>
-                        <Route path={'/:model/:id'} element={<Model/>}/>
+                    {/*    <Route path={'/:model/:id'} element={<Model/>}/>*/}
+                    {/*    <Route path={'/:model/:id'} element={<Model/>}/>*/}
 
-                        <Route path="/*" element={<Error404/>}/>
-                        {/*<Route path="/adidas/*" element={<Error404/>}/>*/}
+                    {/*    <Route path="/*" element={<Error404/>}/>*/}
+                    {/*    <Route path="/adidas/*" element={<Error404/>}/>*/}
 
-                        {/*<Route path={PATH.ERROR404} element={<Error404/>}/>*/}
-                        {/*<Route path="/*" element={<Navigate to={PATH.ERROR404}/>}/>*/}
-                    </Routes>
+                    {/*    <Route path={PATH.ERROR404} element={<Error404/>}/>*/}
+                    {/*    <Route path="/*" element={<Navigate to={PATH.ERROR404}/>}/>*/}
+                    {/*</Routes>*/}
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
